76960e1300033fe43d33f563b2f8476811f41c7e
Denys Dyachenko <d3n3tsu@gmail.com>
2017-01-28 22:56:23 +0200

    Add files via upload

---
+++ Heap.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DijkstraShortPathStandf
+{
+    class Heap
+    {
+        int nodesInHeap = 0;
+        int minNodeIdx = 0;
+        HeapNode[] heap;
+
+        public Heap(Vertix[] vertices)
+        {
+            heap = new HeapNode[vertices.Length - 1];
+            for (int i = 1; i < vertices.Length; i++)
+            {
+                heap[i - 1] = new HeapNode(vertices[i]);
+                heap[i - 1].MyIdx = i - 1;
+                heap[i - 1].ParentIdx = (i / 2) - 1;
+                if ((i * 2) - 1 < heap.Length) heap[i - 1].LeftChildIdx = (i * 2) - 1;
+                else heap[i - 1].LeftChildIdx = -1;
+                if ((i * 2) < heap.Length) heap[i - 1].RightChildIdx = (i * 2);
+                else heap[i - 1].RightChildIdx = -1;
+            }
+            nodesInHeap = heap.Length;
+        }
+
+        public HeapNode Deheapify()
+        {
+            if (heap[minNodeIdx] == null)
+            {
+                return null;
+            }
+            int idxToSwap = FindLeastChildIdx(minNodeIdx);
+            HeapNode nodeToReturn;
+            if (heap[heap[minNodeIdx].LeftChildIdx] == null
+                &&
+                heap[heap[minNodeIdx].RightChildIdx] == null)
+            {
+                nodeToReturn = heap[minNodeIdx];
+                heap[minNodeIdx] = null;
+                return nodeToReturn;
+            }
+            SwapWithParent(minNodeIdx, idxToSwap);
+            nodeToReturn = heap[minNodeIdx];
+            heap[minNodeIdx] = null;
+
+
+            RearrengeWithChild(heap[idxToSwap]);
+
+            int tempMin = -1;
+            for (int i = 0; i < nodeToReturn.MyVertix.Edges.Length; i++)
+            {
+                int idx = nodeToReturn.MyVertix.Edges[i] - 1;
+                if (heap[idx] != null)
+                {
+                    int newDistance = nodeToReturn.ShortestDistance + nodeToReturn.MyVertix.Distances[i];
+                    if (heap[idx].ShortestDistance > newDistance)
+                    {
+                        heap[idx].ShortestDistance = newDistance;
+                    }
+                    RearrengeWithParent(heap[idx]);
+                    tempMin = FindMinNodeIdx(idx);
+
+                }
+            }
+            if (tempMin == -1)
+            {
+                for (int i = 0; i < heap.Length; i++)
+                {
+                    tempMin = FindMinNodeIdx(i);
+                    if (heap[tempMin] != null)
+                    {
+                        break;
+                    }
+                }
+            }
+            minNodeIdx = tempMin;
+
+            nodesInHeap--;
+            return nodeToReturn;
+        }
+
+        int FindMinNodeIdx(int nodeIdx)
+        {
+            if (heap[nodeIdx] == null) return nodeIdx;
+            int parentIdx = heap[nodeIdx].ParentIdx;
+            if (parentIdx == -1) return nodeIdx;
+            if (heap[parentIdx] == null) return parentIdx;
+            else return FindMinNodeIdx(parentIdx);
+        }
+
+        int FindLeastChildIdx(int myIdx)
+        {
+            if (heap[myIdx].RightChildIdx != -1 && heap[heap[myIdx].RightChildIdx] != null)
+            {
+                return FindLeastChildIdx(heap[myIdx].RightChildIdx);
+            }
+            if (heap[myIdx].LeftChildIdx != -1 && heap[heap[myIdx].LeftChildIdx] != null)
+            {
+                return FindLeastChildIdx(heap[myIdx].LeftChildIdx);
+            }
+            return myIdx;
+        }
+
+        bool RearrengeWithChild(HeapNode node)
+        {
+            bool swapped = false;
+            if (node.LeftChildIdx != -1 && heap[node.LeftChildIdx] != null && node.ShortestDistance > heap[node.LeftChildIdx].ShortestDistance)
+            {
+                swapped = true;
+                SwapWithParent(node.MyIdx, node.LeftChildIdx);
+                RearrengeWithChild(heap[node.MyIdx]);
+            }
+            else if (node.RightChildIdx != -1 && heap[node.RightChildIdx] != null && node.ShortestDistance > heap[node.RightChildIdx].ShortestDistance)
+            {
+                swapped = true;
+                SwapWithParent(node.MyIdx, node.RightChildIdx);
+                RearrengeWithChild(heap[node.MyIdx]);
+            }
+            return swapped;
+        }
+
+        void RearrengeWithParent(HeapNode node)
+        {
+            if (node.ParentIdx != -1&& heap[node.ParentIdx]!=null)
+            {
+                int parent = node.ParentIdx;
+                if (RearrengeWithChild(heap[parent]))
+                {
+                    RearrengeWithParent(node);
+                }
+            }
+        }
+
+        void SwapWithParent(int firstIdx, int secondIdx)
+        {
+
+            int firstParent = heap[firstIdx].ParentIdx;
+            int firstLeftChild = heap[firstIdx].LeftChildIdx;
+            int firstRightChild = heap[firstIdx].RightChildIdx;
+
+            int secondParent = heap[secondIdx].ParentIdx;
+            int secondLeftChild = heap[secondIdx].LeftChildIdx;
+            int secondRightChild = heap[secondIdx].RightChildIdx;
+
+            if (secondParent == firstIdx)
+            {
+                heap[firstIdx].ParentIdx = secondIdx;
+            }
+            else
+            { heap[firstIdx].ParentIdx = secondParent; }
+
+            if (secondLeftChild == firstIdx)
+            {
+                heap[firstIdx].LeftChildIdx = secondIdx;
+            }
+            else
+            { heap[firstIdx].LeftChildIdx = secondLeftChild; }
+
+            if (secondRightChild == firstIdx)
+            {
+                heap[firstIdx].RightChildIdx = secondIdx;
+            }
+            else
+            { heap[firstIdx].RightChildIdx = secondRightChild; }
+
+
+
+            if (firstParent == secondIdx)
+            {
+                heap[secondIdx].ParentIdx = firstIdx;
+            }
+            else
+            { heap[secondIdx].ParentIdx = firstParent; }
+
+            if (firstLeftChild == secondIdx)
+            {
+                heap[secondIdx].LeftChildIdx = firstIdx;
+            }
+            else
+            { heap[secondIdx].LeftChildIdx = firstLeftChild; }
+
+            if (firstRightChild == secondIdx)
+            {
+                heap[secondIdx].RightChildIdx = firstIdx;
+            }
+            else
+            { heap[secondIdx].RightChildIdx = firstRightChild; }
+
+
+            bool thereIsParentIdx = heap[firstIdx].ParentIdx != -1 && heap[heap[firstIdx].ParentIdx] != null;
+            if (thereIsParentIdx)
+            {
+                if (heap[heap[firstIdx].ParentIdx].LeftChildIdx == secondIdx)
+                    heap[heap[firstIdx].ParentIdx].LeftChildIdx = firstIdx;
+                else if (heap[heap[firstIdx].ParentIdx].RightChildIdx == secondIdx)
+                    heap[heap[firstIdx].ParentIdx].RightChildIdx = firstIdx;
+            }
+
+            bool thereIsLeftChild = heap[firstIdx].LeftChildIdx != -1 && heap[heap[firstIdx].LeftChildIdx] != null;
+            if (thereIsLeftChild)
+            {
+                heap[heap[firstIdx].LeftChildIdx].ParentIdx = firstIdx;
+            }
+
+            bool thereIsRightChild = heap[firstIdx].RightChildIdx != -1 && heap[heap[firstIdx].RightChildIdx] != null;
+            if (thereIsRightChild)
+            {
+                heap[heap[firstIdx].RightChildIdx].ParentIdx = firstIdx;
+            }
+
+            thereIsParentIdx = heap[secondIdx].ParentIdx != -1 && heap[heap[secondIdx].ParentIdx] != null;
+            if (thereIsParentIdx)
+            {
+                if (heap[heap[secondIdx].ParentIdx].LeftChildIdx == firstIdx)
+                    heap[heap[secondIdx].ParentIdx].LeftChildIdx = secondIdx;
+                else if (heap[heap[secondIdx].ParentIdx].RightChildIdx == firstIdx)
+                    heap[heap[secondIdx].ParentIdx].RightChildIdx = secondIdx;
+            }
+
+            thereIsLeftChild = heap[secondIdx].LeftChildIdx != -1 && heap[heap[secondIdx].LeftChildIdx] != null;
+            if (thereIsLeftChild)
+            {
+                heap[heap[secondIdx].LeftChildIdx].ParentIdx = secondIdx;
+            }
+
+            thereIsRightChild = heap[secondIdx].RightChildIdx != -1 && heap[heap[secondIdx].RightChildIdx] != null;
+            if (thereIsRightChild)
+            {
+                heap[heap[secondIdx].RightChildIdx].ParentIdx = secondIdx;
+            }
+
+        }
+
+        
+    }
+
+    class HeapNode
+    {
+        public Vertix MyVertix;
+        public int MyIdx;
+        public int ShortestDistance;
+        public int RightChildIdx;
+        public int LeftChildIdx;
+        public int ParentIdx;
+
+        public HeapNode(Vertix vertix)
+        {
+            if (vertix.Number == 1)
+            {
+                ShortestDistance = 0;
+            }
+            else
+            {
+                ShortestDistance = int.MaxValue;
+            }
+            MyVertix = vertix;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DijkstraShortPathStandf
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            List<Vertix> listVertices = new List<Vertix>(200);
+            listVertices.Add(new Vertix(0, null, null));
+            string fileName = "";
+            OpenFileDialog opener = new OpenFileDialog();
+            if (opener.ShowDialog() == DialogResult.OK)
+            {
+                fileName = opener.FileName;
+            }
+            if (!string.IsNullOrEmpty(fileName))
+            {
+                using(StreamReader sr = new StreamReader(fileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(new char[] { ' ', '\t' },StringSplitOptions.RemoveEmptyEntries);
+                        int number = int.Parse(line[0]);
+                        int[] edges = new int[line.Length - 1];
+                        int[] distances = new int[line.Length - 1];
+                        for (int i = 1; i < line.Length; i++)
+                        {
+                            string[] edgeInfo = line[i].Split(',');
+                            edges[i - 1] = int.Parse(edgeInfo[0]);
+                            distances[i - 1] = int.Parse(edgeInfo[1]);
+                        }
+                        listVertices.Add(new Vertix(number, edges, distances));
+                    }
+                }
+            }
+            string[] strQuestions = Console.ReadLine().Split(',');
+            int[] questions = new int[strQuestions.Length];
+            for (int i = 0; i < strQuestions.Length; i++)
+            {
+                questions[i] = int.Parse(strQuestions[i]);
+            }
+
+            if (listVertices.Count > 0)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                Dictionary<int, int> ShortestPathes = GetShortestPaths(listVertices.ToArray());
+                watch.Stop();
+                var elapsedMs = watch.ElapsedMilliseconds;
+                
+                Dictionary<int, int> ShortestPathes2 = GetShortestPaths2(listVertices.ToArray());
+                
+
+                int[] answers = new int[questions.Length];
+                int[] answers2 = new int[questions.Length];
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    answers[i] = ShortestPathes[questions[i]];
+                    answers2[i] = ShortestPathes2[questions[i]];
+                    for (int h = 1; h < ShortestPathes.Count - 1; h++)
+                    {
+                        if (ShortestPathes[h] != ShortestPathes2[h])
+                        { throw new Exception("Bad news"); }
+                    }
+                }
+                StringBuilder strToCopy = new StringBuilder();
+                for (int i = 0; i < answers.Length; i++)
+                {
+                    strToCopy.Append(answers[i]);
+                    if (i < answers.Length - 1)
+                    {
+                        strToCopy.Append(",");
+                    }
+                }
+                Clipboard.SetText(strToCopy.ToString());
+            }
+            
+        }
+
+        static Dictionary<int, int> GetShortestPaths2(Vertix[] vertices)
+        {
+            var watch2 = System.Diagnostics.Stopwatch.StartNew();
+            Heap heap = new Heap(vertices);
+            Dictionary<int, int> results = new Dictionary<int, int>();
+            bool doWhile = true;
+            while (doWhile ){
+                HeapNode temp = heap.Deheapify();
+                if (temp != null)
+                {
+                    results.Add(temp.MyVertix.Number, temp.ShortestDistance);
+                }
+                else doWhile = false;
+            }
+
+            watch2.Stop();
+            var elapsedMs2 = watch2.ElapsedMilliseconds;
+            return results;
+        }
+
+        static Dictionary<int, int> GetShortestPaths(Vertix[] vertices)
+        {
+            Vertix[] extractedVertices = new Vertix[vertices.Length];
+            int[] pathes = new int[vertices.Length];
+            extractedVertices[1] = vertices[1];
+            vertices[1].Number = 0;
+            pathes[1] = 0;
+            int count = vertices.Length - 1;
+
+            while (count > 0)
+            {
+                int shortestPath = int.MaxValue;
+                int vertixToExtract = 0;
+                for (int i = 1; i < extractedVertices.Length; i++)
+                {
+                    if (extractedVertices[i].Number != 0)
+                    {
+                        for (int j = 0; j < extractedVertices[i].Edges.Length; j++)
+                        {
+                            int edge = extractedVertices[i].Edges[j];
+                            if (vertices[edge].Number != 0)
+                            {
+                                int tempPath = GetShortestPath(vertices[edge], extractedVertices, pathes);
+                                if (tempPath < shortestPath)
+                                {
+                                    shortestPath = tempPath;
+                                    vertixToExtract = edge;
+                                }
+                            }
+                        }
+                    }
+                }
+                extractedVertices[vertixToExtract] = vertices[vertixToExtract];
+                pathes[vertixToExtract] = shortestPath;
+                vertices[vertixToExtract].Number = 0;
+
+                count--;
+            }
+
+            Dictionary<int, int> result = new Dictionary<int, int>(vertices.Length);
+            for (int i = 0; i < extractedVertices.Length; i++)
+            {
+                result.Add(i, pathes[i]);
+            }
+            return result;
+        }
+
+        static int GetShortestPath(Vertix vertix, Vertix[] extractedVertices, int[] pathes)
+        {
+            int shortestPath = int.MaxValue;
+            for (int i = 1; i < extractedVertices.Length; i++)
+            {
+                int temp = 0;
+                if (extractedVertices[i].Number!=0)
+                {
+                    temp = extractedVertices[i].GetDistance(vertix.Number);
+                    if (temp != -1)
+                    {
+                        int newPath = pathes[i] + temp;
+                        if (newPath <= shortestPath)
+                        {
+                            shortestPath = newPath;
+                        }
+                    }
+                }
+            }
+            
+            return shortestPath;
+        }
+    }
+
+    struct Vertix
+    {
+        public int Number;
+        public int[] Edges;
+        public int[] Distances;
+        public Vertix(int number, int[] edges, int[] distances)
+        {
+            Number = number;
+            Edges = edges;
+            Distances = distances;
+        }
+        
+        public int GetDistance(int vertix)
+        {
+            int idx = Array.IndexOf(Edges, vertix);
+            if (idx != -1) { 
+            return Distances[idx];
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
